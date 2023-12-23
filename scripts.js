513a93b0588027e9d3f67021bfab4f17fa38da7e
Yenny7C <90729562+Yenny7C@users.noreply.github.com>
2023-12-23 03:49:32 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,45 @@
+const ticket = 1000;
+
+// Descuentos
+let descEstudiante = 0.80;
+let descTrainee = 0.50;
+let descJunior = 0.15;
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtenemos los elementos
+    const cantidad = document.getElementById("cantidad");
+    const categoria = document.getElementById("categoria");
+    const totalAPagar = document.getElementById("totalAPagar");
+    const btnResumen = document.getElementById("resumen");
+
+    
+
+    btnResumen.addEventListener("click", function (event) {
+        event.preventDefault(); // Prevenir el comportamiento predeterminado del botón
+        calcularPago(); // Llamar a la función calcularPago
+    });
+
+});
+
+
+
+
+function calcularPago() {
+    let total = cantidad.value * ticket;
+    switch (categoria.value) {
+        case "estudiante":
+            total = total - total * descEstudiante;
+            break;
+        case "trainee":
+            total = total - total * descTrainee;
+            break;
+        case "junior":
+            total = total - total * descJunior;
+            break;
+        default:
+            break;
+    }
+
+    totalAPagar.textContent = `Total a Pagar: $${total}`;
+}
+
